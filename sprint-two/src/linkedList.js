d52a35a3561fa435a0cdb5db08c055d3a247ada7
MKS-Student <mks.student@hackreactor.com>
2015-03-04 17:30:14 -0800

    Complete the "linkedlist"

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,43 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Object.create(Node); // if there is not a head ---- the new tail becomes the head
+    newNode.value = value; // allows list.tail to keep reference of the node
+    newNode.next=null;
+
+    if(list.head === null){
+      list.head = newNode;
+      list.tail = newNode; //allows list.head to keep reference of the node 
+    }
+    else{
+      list.tail.next = newNode;
+      list.tail=newNode;
+    }
   };
 
   list.removeHead = function(){
+    if(list.head === null){
+      return null;
+    } 
+    else{
+        var result = list.head.value;
+        list.head = list.head.next;
+        return result;
+    }
   };
 
   list.contains = function(target){
+    var currentNode = list.head;
+    while(currentNode !== null){
+      if(currentNode.value === target){
+        return true;
+      }
+      else{
+      currentNode = currentNode.next
+      }
+    }
+    return false;
   };
-
   return list;
 };
 
