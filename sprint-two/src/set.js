77e7c73774022af0603b285c1c8e7894e2dfa5e1
MKS-Student <mks.student@hackreactor.com>
2015-03-05 10:52:21 -0800

    Complete "set"

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+	this._storage[item]=true;
 };
 
 setPrototype.contains = function(item){
+	var result=false;
+	if (this._storage[item] === true){
+		result=true;
+	}
+	return result;
 };
 
 setPrototype.remove = function(item){
+	if(this._storage[item]){
+		delete this._storage[item];
+	}
 };
 
 /*
