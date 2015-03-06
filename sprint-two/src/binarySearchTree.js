1a15c5480afd68beed63adcbfe596196dcdcac28
MKS-Student <mks.student@hackreactor.com>
2015-03-05 16:19:08 -0800

    Complete the "Binary Search Tree"

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,86 @@
 var BinarySearchTree = function(value){
+
+	tree= new BTree(value);
+	//tree.root.value=value;
+
+	tree.insert = function(value){
+		var current=tree;
+		var node= new BTree(value);
+		//node.value=value;
+		while (true){
+			//console.log(current);
+			if (value<current.value){
+				if (current.left===null){
+					current.left=node;
+					break;
+				} else {
+					current=current.left;
+				}
+			}
+
+			else if (value >current.value){
+				if (current.right===null){
+					current.right=node;
+					break;
+				}	else {
+					current=current.right;
+				}
+			}
+			else {
+				break;
+			}
+		}
+	};
+
+	tree.contains = function(value){
+		var flag = false;
+		current = tree;
+		while(!flag && current){
+			if(value === current.value){
+				flag = true;
+			} else if(value > current.value){
+				current = current.right;
+			} else if(value < current.value){
+				current = current.left;
+			}
+		}
+
+		return flag;
+	};
+
+	tree.depthFirstLog = function(func){
+		var current=tree;
+		function depthLogRecursive(node){
+			if (node !==null){// if the current node exist
+				func(node.value);
+				depthLogRecursive(node.left);
+				depthLogRecursive(node.right);
+			}
+			else { // if the current node doesn't exit
+				return;
+				
+			}
+		}
+		depthLogRecursive(current);
+	};
+	return tree;
 };
 
+	var BTree = function(value){
+		this.value=value;
+		this.left=null;
+		this.right=null;
+	}
+/*
+var Node = function(value){
+  
+
+  this.value = value;
+  this.left=null;
+  this.right=null;
+
+};
+*/
 
 /*
  * Complexity: What is the time complexity of the above functions?
